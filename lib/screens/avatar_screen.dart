6dc874bb3998081a29718bf22f31dbdf77fa035a
JohanRiv <johancamiloandres@hotmail.com>
2022-03-23 23:22:44 -0500

    Add container avatar on avatar screen

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -17,11 +17,30 @@ class AvatarScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: CircleAvatar(
-          backgroundImage: NetworkImage(
-              'https://www.nautiljon.com/images/perso/00/64/bojji_20546.jpg'),
-          maxRadius: 110,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.nautiljon.com/images/perso/00/64/bojji_20546.jpg'),
+              maxRadius: 110,
+            ),
+            Container(
+              width: 220,
+              height: 220,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(110)),
+              child: const FadeInImage(
+                image: NetworkImage(
+                    'https://www.nautiljon.com/images/perso/00/64/bojji_20546.jpg'),
+                placeholder: AssetImage('assets/images/gray_background.jpg'),
+                fit: BoxFit.cover,
+                fadeInDuration: Duration(milliseconds: 300),
+              ),
+              clipBehavior: Clip.antiAlias,
+            )
+          ],
         ),
       ),
     );
