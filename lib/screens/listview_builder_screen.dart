7266474f1fa3fae44d40fd795ac45a325ba511d0
JohanRiv <johancamiloandres@hotmail.com>
2022-03-30 23:19:02 -0500

    Adding charge effect to infinite scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,3 +1,4 @@
+import 'package:components_app/themes/themes.dart';
 import 'package:flutter/material.dart';
 
 class ListviewBuilderScreen extends StatefulWidget {
@@ -12,6 +13,8 @@ class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
   final ScrollController scrollController =
       ScrollController(); // Controlador del list view, permite saber el estado del mismo
 
+  bool isLoading = false; // Control de la carga
+
   double positionScroll = 0;
   double totalScroll = 0;
 
@@ -21,23 +24,35 @@ class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
     super.initState();
     // Adición de listener al listview
     scrollController.addListener(() {
-      positionScroll = scrollController.position.pixels;
-      totalScroll = scrollController.position.maxScrollExtent;
       // Con scrollController.position.pixels se puede saber la posición actual del scroll
       // Con scrollController.position.maxScrollExtent se puede saber la posición final del scroll
-      print('${positionScroll}, ${totalScroll}');
+      positionScroll = scrollController.position.pixels;
+      totalScroll = scrollController.position.maxScrollExtent;
       if (positionScroll >= (totalScroll - 600)) {
-        addImagesListView();
-        setState(() {});
-        print('Charge More');
+        fetchData();
       }
     });
+  }
+
+  Future fetchData() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 3));
+    addImagesListView();
+    isLoading = false;
+    setState(() {});
+    if (positionScroll >= (totalScroll - 100)) {
+      scrollController.animateTo(totalScroll + 100,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.fastOutSlowIn);
+      setState(() {});
+    }
   }
 
   void addImagesListView() {
     final lastImageId = imagesId.last;
     imagesId.addAll([1, 2, 3, 4, 5].map((e) => lastImageId + e));
-    print(imagesId);
   }
 
   @override
@@ -52,29 +67,60 @@ class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
           context: context, // Siempre se debe compartir el context
           removeTop: true, // Se remueve el padding superior
           removeBottom: true, // Remueve el padding inferior
-          child: ListView.builder(
-            physics:
-                const BouncingScrollPhysics(), // Adiciona efectos nativos al scroll
-            controller: scrollController,
-            itemCount: imagesId.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: [
-                  FadeInImage(
-                      width: double.infinity,
-                      height: 350,
-                      fit: BoxFit.cover,
-                      placeholder:
-                          const AssetImage('assets/images/gray_background.jpg'),
-                      image: NetworkImage(
-                          'https://picsum.photos/500/350?image=${imagesId[index]}')),
-                  const SizedBox(
-                    height: 20,
-                  )
-                ],
-              );
-            },
+          child: Stack(
+            children: [
+              ListView.builder(
+                physics:
+                    const BouncingScrollPhysics(), // Adiciona efectos nativos al scroll
+                controller: scrollController,
+                itemCount: imagesId.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    children: [
+                      FadeInImage(
+                          width: double.infinity,
+                          height: 350,
+                          fit: BoxFit.cover,
+                          placeholder: const AssetImage(
+                              'assets/images/gray_background.jpg'),
+                          image: NetworkImage(
+                              'https://picsum.photos/500/350?image=${imagesId[index]}')),
+                      const SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  );
+                },
+              ),
+              // Mostrar u ocultar un widget con un condicional
+              if (isLoading)
+                Positioned(
+                    bottom: 40,
+                    left: MediaQuery.of(context).size.width * 0.5 - 35,
+                    child: const LoadingPanel())
+            ],
           ),
         ));
+  }
+}
+
+// Widget para mostrar un loading
+class LoadingPanel extends StatelessWidget {
+  const LoadingPanel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: DarkTheme.primaryBackground.withOpacity(0.9),
+      ),
+      child: const CircularProgressIndicator.adaptive(),
+    );
   }
 }
